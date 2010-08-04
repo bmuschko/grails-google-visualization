07a9be8199346f6b848d0286b680a9c9b3f3135b
bmuschko <bmuschko@832c1c66-4827-0410-8465-ccb17913cd09>
2010-08-04 11:14:34 +0000

    Upgraded version number.

--- GoogleVisualizationGrailsPlugin.groovy
+++ GoogleVisualizationGrailsPlugin.groovy
@@ -16,7 +16,7 @@
  */
 class GoogleVisualizationGrailsPlugin {
     // the plugin version
-    def version = "0.2.1"
+    def version = "0.2.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
