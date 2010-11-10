b3ddf9ea8e7b27eb8429863b81f5ae372dcf7b22
bmuschko <bmuschko@832c1c66-4827-0410-8465-ccb17913cd09>
2010-11-10 02:36:18 +0000

    Adapted to new core chart attributes and events.

--- GoogleVisualizationGrailsPlugin.groovy
+++ GoogleVisualizationGrailsPlugin.groovy
@@ -16,7 +16,7 @@
  */
 class GoogleVisualizationGrailsPlugin {
     // the plugin version
-    def version = "0.2.3"
+    def version = "0.2.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
