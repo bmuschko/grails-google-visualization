5c28215ab74975af2ce6a5d96e54b08b948e8f87
Angel Ruiz <aruizca@gmail.com>
2014-12-09 08:41:54 +1100

    The Google Visualization Javascript API should be accessed through HTTPS Next version will be 1.0 since there are some backward breaking changes

--- GoogleVisualizationGrailsPlugin.groovy
+++ GoogleVisualizationGrailsPlugin.groovy
@@ -15,7 +15,7 @@
  * @author <a href='mailto:benjamin.muschko@gmail.com'>Benjamin Muschko</a>
  */
 class GoogleVisualizationGrailsPlugin {
-    def version = "0.8-SNAPSHOT"
+    def version = "1.0-SNAPSHOT"
     def grailsVersion = "1.2 > *"
     def author = "Benjamin Muschko"
     def authorEmail = "benjamin.muschko@gmail.com"
