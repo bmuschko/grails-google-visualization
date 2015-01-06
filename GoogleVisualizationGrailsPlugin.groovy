ee561c4ac7d57866b8b6bec2539b9fed5db7d5a2
Angel Ruiz <aruizca@gmail.com>
2015-01-07 08:52:48 +1100

    Sets new development version to 1.0.2-SNAPSHOT

--- GoogleVisualizationGrailsPlugin.groovy
+++ GoogleVisualizationGrailsPlugin.groovy
@@ -15,7 +15,7 @@
  * @author <a href='mailto:benjamin.muschko@gmail.com'>Benjamin Muschko</a>
  */
 class GoogleVisualizationGrailsPlugin {
-    def version = "1.0.1"
+    def version = "1.0.2-SNAPSHOT"
     def grailsVersion = "1.2 > *"
     def author = "Benjamin Muschko"
     def authorEmail = "benjamin.muschko@gmail.com"
