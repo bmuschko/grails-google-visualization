c8a4830fa33e9555bc547e0b41d1365d8f716773
mansiarora <mansi@intelligrape.com>
2015-06-27 12:53:11 +0530

    Fixed test cases for ObjectRenderer;

--- src/test/groovy/org/grails/plugins/google/visualization/data/renderer/ObjectRendererTests.groovy
+++ src/test/groovy/org/grails/plugins/google/visualization/data/renderer/ObjectRendererTests.groovy
@@ -23,15 +23,19 @@ import spock.lang.Specification
  */
 class ObjectRendererTests extends Specification {
     void testRenderValue() {
-        assertEquals "{stroke: 'black', fill: '#eee', strokeSize: 1}", ObjectRenderer.instance.renderValue(new Expando(stroke:'black', fill:'#eee', strokeSize: 1)).toString()
+        expect:
+        ObjectRenderer.instance.renderValue(new Expando(stroke: 'black', fill: '#eee', strokeSize: 1)).toString() == "{stroke: 'black', fill: '#eee', strokeSize: 1}"
     }
 
     void testRenderValueRealObject() {
+        setup:
         def simpleObject = new SimpleClass(a: "xxx", b: 123)
+        when:
         def renderedObject = ObjectRenderer.instance.renderValue(simpleObject)
-        assertEquals "{b: 123, a: 'xxx'}", renderedObject.toString()
+        then:
+        renderedObject.toString() == "{b: 123, a: 'xxx'}"
     }
-	
+
     class SimpleClass {
         String a
         int b
